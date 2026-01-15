19acba34a7105530dc3872dd3289770d9f7184cd
Leandro Funes <leofunesar@gmail.com>
2026-01-15 11:18:55 +0000

    Finished search tasks project, isn't still working, I have to continue with debuggin helping myself with the console f12

--- assets/js/07-search_tasks.js
+++ assets/js/07-search_tasks.js
@@ -2,16 +2,16 @@
 
 const inputTask = document.querySelector(`#input_task`);
 const newTask = document.querySelector(`#add_task_btn`);
-const searchTask = document.querySelector(`#search_task`);
+const inputSearch = document.querySelector(`#search_task`);
 const btnSearch = document.querySelector(`#btn_search_task`);
 const tableBody = document.querySelector(`#table_body`);
 const selectPriority = document.querySelector(`#select_priority`)
 const countTasks = document.querySelector(`#count_tasks`);
 
 const tasks = [
-    {id: "id-001", task: "test task 1", priority: "red"},
+    /* {id: "id-001", task: "test task 1", priority: "red"},
     {id: "id-002", task: "test task 2", priority: 'yellow'},
-    {id: "id-003", task: "test task 3", priority: 'green'}
+    {id: "id-003", task: "test task 3", priority: 'green'} */
 ];
 
 
@@ -29,15 +29,16 @@ function renderTable(){
                 <td>
                     <span 
                     style="
-                        color:${task.priority}; border-radius: 50%; 
-                        height: 5px; 
+                        color:${task.priority};
+                        border-radius: 50%;
+                        height: 5px;
                         width: 5px">
                     </span> 
                     <button onclick="DeleteTasks(${task.id})" class="btnDelete">
                     x
                     </button>
                 </td>
-            </tr>           
+            </tr>
             `
     };
 };
@@ -49,19 +50,27 @@ newTask.addEventListener(`click`, () => {
     
     inputTask.value = "";
 
-    renderTable();
-
+    renderTable(tasks);
+    
     countTasks.innerHTML = tasks.length;
     tableBody.innerHTML = HTMLcontent;
-
 });
+
 
 function DeleteTasks(id){
     const index = tasks.findIndex((element) => element.id == id)
     tasks.splice(index, 1)
 
-   renderTable();
+   renderTable(tasks);
 
     countTasks.innerHTML = tasks.length;
     tableBody.innerHTML = HTMLcontent;
 };
+
+
+btnSearch.addEventListener(`click`, () => {
+    const fetchedTask = inputSearch.value;
+    const filteredTask = tasks.filter(
+        (task) => task.task.includes(fetchedTask)
+    )
+});
